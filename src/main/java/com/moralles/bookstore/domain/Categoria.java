e664e56220565b411799ec3042a5c4e169112e9d
thiagomoralles <thiagomoralles@outlook.com>
2021-06-09 18:03:51 -0300

    Finalizando classes de dominio

--- src/main/java/com/moralles/bookstore/domain/Categoria.java
+++ src/main/java/com/moralles/bookstore/domain/Categoria.java
@@ -4,19 +4,31 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="Categoria")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livros> livros = new ArrayList<>();
 }
--- src/main/java/com/moralles/bookstore/domain/Livros.java
+++ src/main/java/com/moralles/bookstore/domain/Livros.java
@@ -2,16 +2,36 @@ package com.moralles.bookstore.domain;
 
 import java.io.Serializable;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Livros")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Livros implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(name = "titulo")
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 }
